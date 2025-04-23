97e72b6c26d9e3d6d2559682788adcb167ce1c3b
Wilfredo <wilfredopintomata@gmail.com>
2025-04-23 20:15:40 +0200

    Excuse generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,42 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+const generateRandomNumber = (max)=>{
+  return Math.floor(Math.random() * max)
+}
+
+const generateExcuse = () => {
+  let num;
+  let excuse = '';
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  for (let index = 0; index < 4; index++) {
+    switch (index) {
+      case 0:
+        num =generateRandomNumber(who.length);
+        excuse += ' ' + who[num];
+        break;
+      case 1:
+        num = generateRandomNumber(action.length);
+        excuse += ' ' + action[num];
+        break;
+      case 2:
+        num = generateRandomNumber(what.length);
+        excuse += ' ' + what[num];
+        break;
+      case 3:
+        num = generateRandomNumber(when.length);
+        excuse += ' ' + when[num];
+        break;
+    }
+  }
+  return excuse
+}
+
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let excuse = generateExcuse();
+  document.getElementById('excuse').innerHTML = excuse;
 };
